ff693e5c2b7163633fab862c8bc5a21c4fff14b4
thuyhoang-hvtt <thuyhoang.hvtt@gmail.com>
2022-03-21 18:47:31 +0700

    🏷️  chore(type): declare type of meta env

--- workspaces/client/src/vite-env.d.ts
+++ workspaces/client/src/vite-env.d.ts
@@ -2,3 +2,12 @@
 interface Window {
   ethereum: any;
 }
+
+interface ImportMetaEnv {
+  readonly VITE_SMART_CONTRACT_ADDRESS: string;
+  // more env variables...
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
