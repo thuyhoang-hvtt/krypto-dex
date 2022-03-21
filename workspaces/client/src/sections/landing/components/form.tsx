c252f20e4d7433e9c1241d6c92f99ef6474f5b00
thuyhoang-hvtt <thuyhoang.hvtt@gmail.com>
2022-03-21 18:59:18 +0700

    ✨ feat(abi): integrate with contract abi

--- workspaces/client/src/sections/landing/components/form.tsx
+++ workspaces/client/src/sections/landing/components/form.tsx
@@ -1,22 +1,46 @@
-import { useEffect, useState } from 'react';
+import React from 'react';
 import { Loader } from '@/components';
 import Input from './input';
 
-function Form() {
-  const [isLoading, setIsLoading] = useState(true);
+interface IProps {
+  isLoading: boolean;
+  isConnected: boolean;
+  data: any;
+  onInputChange: (event: React.ChangeEvent<HTMLInputElement>, target: string) => void;
+  onSubmit: () => void;
+}
 
-  useEffect(() => {
-    const timeout = setTimeout(() => setIsLoading(false), 10000);
-
-    return () => clearTimeout(timeout);
-  }, []);
-
+function Form({ isConnected, isLoading, data, onInputChange, onSubmit }: IProps) {
   return (
     <div className="p-5 sm:w-96 w-full flex flex-col justify-start items-center blue-glassmorphism">
-      <Input placeholder="Address To" name="addressTo" type="text" />
-      <Input placeholder="Amount (ETH)" name="amount" type="number" />
-      <Input placeholder="Keyword (Gif)" name="keyword" type="text" />
-      <Input placeholder="Enter Message" name="message" type="text" />
+      <Input
+        placeholder="Address To"
+        name="addressTo"
+        type="text"
+        onChange={onInputChange}
+        value={data.addressTo}
+      />
+      <Input
+        placeholder="Amount (ETH)"
+        name="amount"
+        type="number"
+        onChange={onInputChange}
+        value={data.amount}
+      />
+      <Input
+        placeholder="Keyword (Gif)"
+        name="keyword"
+        type="text"
+        onChange={onInputChange}
+        value={data.keyword}
+      />
+      <Input
+        placeholder="Enter Message"
+        name="message"
+        type="text"
+        onChange={onInputChange}
+        value={data.message}
+      />
 
       <div className="h-[1px] w-full bg-gray-400 my-2" />
 
@@ -25,8 +49,9 @@ function Form() {
       ) : (
         <button
           type="button"
-          onClick={() => null}
-          className="text-white w-full mt-2 border-[1px] p-2 border-[#3d4f7c] hover:bg-[#3d4f7c] rounded-full cursor-pointer"
+          onClick={onSubmit}
+          className="text-white w-full mt-2 border-[1px] p-2 border-[#3d4f7c] hover:bg-[#3d4f7c] rounded-full cursor-pointer disabled:hover:bg-transparent disabled:cursor-not-allowed"
+          disabled={!isConnected}
         >
           Send now
         </button>
--- workspaces/client/src/sections/landing/landing.tsx
+++ workspaces/client/src/sections/landing/landing.tsx
@@ -1,23 +1,35 @@
 import { Navbar } from '@/components';
-import EthCard from './components/card';
+import DigitalCard from './components/card';
 import Form from './components/form';
 import Mission from './components/mission';
 import Connection from './components/connection';
+import useHooks from './hooks';
 
 function LandingSection() {
+  const { state, handler } = useHooks();
+
   return (
     <section id="home" className="gradient-bg-welcome min-h-screen">
       <Navbar />
       <div className="flex w-full justify-center items-center">
         <div className="flex mf:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
           <div className="flex flex-1 justify-start items-start flex-col mf:mr-10">
-            <Connection />
+            <Connection
+              walletAddress={state.walletAddress}
+              handleConnectWallet={handler.handleConnectWallet}
+            />
             <Mission />
           </div>
 
           <div className="flex flex-col flex-1 items-center justify-start w-full mf:mt-0 mt-10">
-            <EthCard walletAddress="" />
-            <Form />
+            <DigitalCard tokenName={state.tokenName} walletAddress={state.walletAddress} />
+            <Form
+              isConnected={state.connected}
+              isLoading={state.loading}
+              data={state.form}
+              onInputChange={handler.handleInputChange}
+              onSubmit={handler.handleTransferToken}
+            />
           </div>
         </div>
       </div>
